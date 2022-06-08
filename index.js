c23aae14a6fef98a352976e789c56310c14f43fd
abdalla.jama <abdallajama201@gmail.com>
2022-06-08 19:54:21 -0400

    all functionality complete

--- index.js
+++ index.js
@@ -1,13 +1,85 @@
-// TODO: Include packages needed for this application
+//packages needed for this application
+const generateMD = require("./utils/generateMarkdown");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 
-// TODO: Create an array of questions for user input
-const questions = [];
+//array of questions for user input
+const questions = [
+    "What is the title of your project?",
+    "Please describe your project:",
+    "What are the installation instructions for your project, if any?",
+    "What usage information would you like to provide the user, if any?",
+    "What are the contribution guidelines for your project, if any?",
+    "What are the testing instructions for your project, if any?",
+    "Please choose a license:",
+    "Please enter your GitHub username:",
+    "Please enter your email address:",
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const names = [
+    "title",
+    "description",
+    "installation",
+    "usage",
+    "contribution",
+    "testing",
+    "license",
+    "github",
+    "email",
+];
 
-// TODO: Create a function to initialize app
-function init() {}
+let prompts = [];
+
+// Builds the one prompt with multiple options
+function licensePrompt() {
+    let licenseOption = ["Apache","BSD","Creative Commons","GNU","MIT","Zlib","none"];
+    let licensePrompt = {
+        type: "list",
+        message: questions[6],
+        name: names[6],
+        choices: licenseOption,
+    }
+    return licensePrompt;
+}
+
+// Builds objects for prompt functions
+function populatePrompts() {
+    for(let i = 0; i < questions.length; i++) {
+        if(i !== 6) {
+            let ques = {
+                type: "input",
+                message: questions[i],
+                name: names[i]
+            }
+            prompts.push(ques);
+        } else {
+            prompts.push(licensePrompt());
+        }    
+    }
+}
+
+// Initiates prompts
+function askQuestions() {
+    const inquirer = require("inquirer");
+    inquirer
+    .prompt(prompts)
+    .then((response) => {
+        writeToFile(response);
+    });
+}
+
+// writes README file
+function writeToFile(data) {
+    //CHANGE LATER
+    fs.writeFile("README1.md",generateMarkdown(data), (err) =>
+    err ? console.error(err) : "");
+}
+
+// function to initialize app
+function init() {
+    populatePrompts();
+    askQuestions();
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,84 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// function that returns a license badge based on which license is passed in
+// Function will not be called if no license chosen
+function renderLicenseBadge(license) {
+    let licenseOption = ["Apache","BSD","Creative Commons","GNU","MIT","Zlib",];
+    let badges = [
+      "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)",
+      "[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)",
+      "[![License: CC0-1.0](https://img.shields.io/badge/License-CC0_1.0-lightgrey.svg)](http://creativecommons.org/publicdomain/zero/1.0/)",
+      "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)",
+      "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)",
+      "[![License: Zlib](https://img.shields.io/badge/License-Zlib-lightgrey.svg)](https://opensource.org/licenses/Zlib)",      
+    ]
+    for(let i = 0; i < licenseOption.length; i++) {
+      if(license === licenseOption[i]) {
+        return badges[i];
+      }
+    }
+}
 
 // TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// function will not be called if no license chosen
+function renderLicenseLink(license) {
+	let licenseOption = ["Apache","BSD","Creative Commons","GNU","MIT","Zlib",];
+	for(let i = 0; i < licenseOption.length; i++) {
+		if(license === licenseOption[i]) {
+		return `[${licenseOption[i]}](./LICENSE.md)`;
+		}
+	}
+} 
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
+// generates markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+	const {title,description,installation,usage,contribution,testing,license,github,email} = data;
+	const sections = [title,description,installation,usage,contribution,testing,license,github,email];
+	const headings = ["title","Description","Installation","Usage","Contributing","Tests","License","Questions"];
+	
+	let markDown = "";
+	let tableOfContents = "## Table of Contents\n";
+	
+	// Populates Title
+	if(sections[0]) {
+		markDown += `# ${sections[0]}\n`;
+	}
+	// Populates badge or just leaves space
+	if(sections[6] !== "none") {
+		markDown += `${renderLicenseBadge(license)}\n\n`;
+	}else {
+		markDown += `\n\n`;
+	}
+    // Populates middle section
+    for (let i = 1; i < headings.length - 2; i++) {
+		if(sections[i]) {
+			markDown += `## ${headings[i]}\n\n${sections[i]}\n\n`;
+			tableOfContents += `- [${headings[i]}](#${headings[i].toLowerCase()})\n`
+		}
+		// Leaves marker for table of contents
+		if(i === 1) {
+				markDown += `|\n`;
+		}   
+    }
+    // Populates license section
+    if(sections[6] !== "none") {
+        markDown += `## ${headings[6]}\n\nThis project is under the ${license} license at ${renderLicenseLink(license)}\n\n`;
+        tableOfContents += `- [${headings[6]}](#${headings[6].toLowerCase()})\n`
+    }
+    // Populates License section
+    if(sections[7] || sections[8]) {
+        markDown += `## ${headings[7]}\n\nIf you have additional questions please feel free to reach out.\n\n`;
+        tableOfContents += `- [${headings[7]}](#${headings[7].toLowerCase()})\n`
+        if(sections[7]) {
+            markDown += `Github: https://www.github.com/${sections[7]}\n\n`;
+        }
+        if(sections[8]) {
+            markDown += `Email: ${sections[8]}`
+        }
+    }
+    // Inserts table of contents into middle
+    let portions = markDown.split("|");
+    markDown = `${portions[0]}${tableOfContents}\n\n${portions[1]}`;
 
-`;
+  	return markDown;
 }
 
 module.exports = generateMarkdown;
